6e1c43f135f5478bdc59c8a167bcaf42746a0522
Joel Holdsworth <joel@airwebreathe.org.uk>
2013-05-06 09:49:29 +0100

    Added project and synthesis build infrastructure
    This code has been adapted from minsoc - and LGPL 3.0 project
    maintained by Raul Fajardo.

---
+++ prj/xilinx/prj.mk
@@ -0,0 +1,49 @@
+#------------------------------------------------------------------------------
+#
+# Copyright (C) 2013 Joel Holdsworth
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+#
+#------------------------------------------------------------------------------
+
+PROJECTS = $(PROJECT_NAME)_top.prj
+SRC_DIR = $(ROOT_DIR)/prj/src
+SCRIPTS_DIR = $(ROOT_DIR)/prj/scripts
+XILINX_PRJ_DIR = $(ROOT_DIR)/prj/xilinx
+
+XILINX_PRJ_FILES = $(addprefix $(XILINX_PRJ_DIR)/, \
+	$(addsuffix .prj, $(basename $(PROJECTS))))
+XILINX_XST_FILES = $(addprefix $(XILINX_PRJ_DIR)/, \
+	$(addsuffix .xst, $(basename $(PROJECTS))))
+
+#all: $(XILINX_PRJ_FILES) $(XILINX_XST_FILES)
+
+#clean:
+#	rm -rf $(XILINX_PRJ_DIR)/*.prj  $(XILINX_PRJ_DIR)/*.xst
+
+
+$(XILINX_PRJ_DIR)/$(PROJECT_NAME)_top.xst: $(SRC_DIR)/$(PROJECT_NAME)_top.prj
+	bash $(SCRIPTS_DIR)/xilinxxst.sh $(ROOT_DIR) $^ $@ \
+		$(PROJECT_NAME)_top.prj $(PROJECT_NAME)_top topmodule \
+		$(DEVICE_PART)
+
+$(XILINX_PRJ_DIR)/$(PROJECT_NAME)_top.prj: $(SRC_DIR)/$(PROJECT_NAME)_top.prj
+	bash $(SCRIPTS_DIR)/xilinxprj.sh $(ROOT_DIR) $^ $@ topmodule
+
+$(XILINX_PRJ_DIR)/%.xst: $(SRC_DIR)/%.prj
+	bash $(SCRIPTS_DIR)/xilinxxst.sh $(ROOT_DIR) $^ $@ $*.prj $*
+
+$(XILINX_PRJ_DIR)/%.prj: $(SRC_DIR)/%.prj
+	bash $(SCRIPTS_DIR)/xilinxprj.sh $(ROOT_DIR) $^ $@
---
+++ syn/xilinx/syn.mk
@@ -0,0 +1,43 @@
+#------------------------------------------------------------------------------
+#
+# Copyright (C) 2013 Joel Holdsworth
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+#
+#------------------------------------------------------------------------------
+
+include $(ROOT_DIR)/prj/xilinx/prj.mk
+
+PROJECT_DIR=$(ROOT_DIR)/prj/xilinx
+
+$(PROJECT_NAME)_top.ngc: ${RTL_DIR}/*.v $(PROJECT_DIR)/$(PROJECT_NAME)_top.xst \
+	$(PROJECT_DIR)/$(PROJECT_NAME)_top.prj
+	make prepare
+	xst -ifn "$(PROJECT_DIR)/$(PROJECT_NAME)_top.xst"
+
+$(PROJECT_NAME).ngd: $(ROOT_DIR)/$(CONSTRAINT_FILE) $(PROJECT_NAME)_top.ngc
+	ngdbuild -p ${DEVICE_PART} -uc $(ROOT_DIR)/$(CONSTRAINT_FILE) -aul \
+	$(PROJECT_NAME)_top.ngc $(PROJECT_NAME).ngd
+
+$(PROJECT_NAME).ncd: $(PROJECT_NAME).ngd
+	map -bp -timing -cm speed -equivalent_register_removal on \
+	-logic_opt on -ol high -power off -register_duplication on \
+	-retiming on -w -xe n $(PROJECT_NAME).ngd
+
+$(PROJECT_NAME)_par.ncd: $(PROJECT_NAME).ncd
+	par -ol high -w -xe n $(PROJECT_NAME).ncd $(PROJECT_NAME)_par.ncd
+
+$(PROJECT_NAME).bit: $(PROJECT_NAME)_par.ncd
+	bitgen -d -w $(PROJECT_NAME)_par.ncd $(PROJECT_NAME).bit
